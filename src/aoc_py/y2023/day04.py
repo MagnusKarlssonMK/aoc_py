2531f596f0160af36cc56377ed8f6d73aef78fb9
MagnusKarlssonMK <magnus.karlsson.mk@gmail.com>
2026-08-29 14:56:08 +0200

    2023 day 4 migrated.

---
+++ src/aoc_py/y2023/day04.py
@@ -0,0 +1,37 @@
+"""
+2023 day 4 - Scratchcards
+"""
+class Card:
+    def __init__(self, s: str) -> None:
+        all_numbers = s.split(": ")[1]
+        parts = all_numbers.split(" | ")
+        winning_numbers = {int(p) for p in parts[0].split()}
+        draw_numbers = {int(c) for c in parts[1].split()}
+        self.wincount: int = len(winning_numbers & draw_numbers)
+        self.score: int = 0 if self.wincount <= 0 else pow(2, self.wincount - 1)
+
+
+class InputData:
+    def __init__(self, s: str) -> None:
+        self.__scratchcards = [Card(line) for line in s.splitlines()]
+
+    def get_p1(self) -> int:
+        return sum([card.score for card in self.__scratchcards])
+
+    def get_p2(self) -> int:
+        copylist = [1 for _ in range(len(self.__scratchcards))]
+        for i, card in enumerate(self.__scratchcards):
+            for j in range(1, card.wincount + 1):
+                copylist[i + j] += copylist[i]
+        return sum(copylist)
+
+
+def solve_parts(inputdata: str, part: int | None = None) -> tuple[str, str]:
+        p1, p2 = "-1"
+        p = InputData(inputdata)
+        if part in (None, 1):
+            p1 = str(p.get_p1())
+        if part in (None, 2):
+            p2 = str(p.get_p2())
+
+        return p1, p2
--- tests/y2023/test_day04.py
+++ tests/y2023/test_day04.py
@@ -1,25 +1,21 @@
-from day04 import InputData
-
-
-TEST_DATA = """Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+TEST_STRING = """Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
 Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
 Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
 Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
 Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
 Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"""
 
+from aoc_py.y2023.day04 import solve_parts
 
 # ----------- Part 1 ------------
 
-
 def test_part1_1() -> None:
-    test_input = InputData(TEST_DATA)
-    assert test_input.solve_part1() == 13
+    p1, _ = solve_parts(TEST_STRING, 1)
+    assert p1 == "13"
 
 
 # ----------- Part 2 ------------
 
-
 def test_part2_1() -> None:
-    test_input = InputData(TEST_DATA)
-    assert test_input.solve_part2() == 30
+    _, p2 = solve_parts(TEST_STRING, 2)
+    assert p2 == "30"
