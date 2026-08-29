e0890e41dbaca707940789ead09d04516c5a6a82
MagnusKarlssonMK <magnus.karlsson.mk@gmail.com>
2026-08-29 17:31:40 +0200

    2023 day 9 migrated.

--- src/aoc_py/y2023/day09.py
+++ src/aoc_py/y2023/day09.py
@@ -1,12 +1,9 @@
 """
+2023 day 9 - Mirage Maintenance
+
 Just a simple recursive function to generate the lines until a line of zeroes shows up.
 Surprisingly simple part 2, basically just to reverse the input data and run the same function.
 """
-
-import time
-from pathlib import Path
-
-
 def findnextnumber(nbrs: list[int]) -> int:
     if all(nbr == 0 for nbr in nbrs):
         return 0
@@ -25,18 +22,12 @@ def get_numbers(s: str) -> tuple[int, int]:
     return result_p1, result_p2
 
 
-def main(aoc_input: str) -> None:
-    p1, p2 = get_numbers(aoc_input)
-    print(f"Part1: {p1}")
-    print(f"Part2: {p2}")
+def solve_parts(inputdata: str, part: int | None = None) -> tuple[str, str]:
+        p1, p2 = "-1"
+        p = get_numbers(inputdata)
+        if part in (None, 1):
+            p1 = str(p[0])
+        if part in (None, 2):
+            p2 = str(p[1])
 
-
-if __name__ == "__main__":
-    ROOT_DIR = Path(Path(__file__).parents[1], "AdventOfCode-Input")
-    INPUT_FILE = Path(ROOT_DIR, "2023/day09.txt")
-
-    start_time = time.perf_counter()
-    with open(INPUT_FILE, "r") as file:
-        main(file.read().strip("\n"))
-    end_time = time.perf_counter()
-    print(f"Total time (ms): {1000 * (end_time - start_time)}")
+        return p1, p2
---
+++ tests/y2023/test_day09.py
@@ -0,0 +1,18 @@
+TEST_STRING = """0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45"""
+
+from aoc_py.y2023.day09 import solve_parts
+
+# ----------- Part 1 ------------
+
+def test_part1_1() -> None:
+    p1, _ = solve_parts(TEST_STRING, 1)
+    assert p1 == "114"
+
+
+# ----------- Part 2 ------------
+
+def test_part2_1() -> None:
+    _, p2 = solve_parts(TEST_STRING, 2)
+    assert p2 == "2"
