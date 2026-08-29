70121566a4722a0f3adc7449bca355dbaea9277d
MagnusKarlssonMK <magnus.karlsson.mk@gmail.com>
2026-08-29 14:05:50 +0200

    2023 day 1 migrated.

--- src/aoc_py/y2023/day01.py
+++ src/aoc_py/y2023/day01.py
@@ -1,12 +1,10 @@
 """
+2023 day 1 - Trebuchet?!
+
 For part 1, simply extract all numbers from the string and combine the first and last digit.
 For part 2, first pre-process the string by replacing spelled out digits with the same word but with the actual digit
 inserted in the middle, in a place where it won't destroy the string in case numbers are overlapping (e.g. 'threeight').
 """
-import time
-from pathlib import Path
-
-
 class Calibrationstring:
     def __init__(self, inputstr: str) -> None:
         self.__calstring = inputstr
@@ -16,13 +14,13 @@ class Calibrationstring:
                   'six': 'si6x', 'seven': 'se7ven', 'eight': 'eig8th', 'nine': 'ni9ne'}
         strcopy = self.__calstring
         if spelled_out:
-            for digit in digits:
-                strcopy = strcopy.replace(digit, digits[digit])
+            for olddigit, newname in digits.items():
+                strcopy = strcopy.replace(olddigit, newname)
         digits = [char for char in strcopy if char.isdigit()]
         return 0 if len(digits) == 0 else int(digits[0] + digits[-1])
 
 
-class Document:
+class InputData:
     def __init__(self, rawstr: str) -> None:
         self.__values = [Calibrationstring(line) for line in rawstr.splitlines()]
 
@@ -30,18 +28,12 @@ class Document:
         return sum([v.getcalibrationvalue(spelled_out) for v in self.__values])
 
 
-def main(aoc_input: str) -> None:
-    document = Document(aoc_input)
-    print(f"Part 1: {document.get_calibration_sum()}")
-    print(f"Part 2: {document.get_calibration_sum(True)}")
+def solve_parts(inputdata: str, part: int | None = None) -> tuple[str, str]:
+        p1, p2 = "-1"
+        p = InputData(inputdata)
+        if part in (None, 1):
+            p1 = str(p.get_calibration_sum())
+        if part in (None, 2):
+            p2 = str(p.get_calibration_sum(True))
 
-
-if __name__ == "__main__":
-    ROOT_DIR = Path(Path(__file__).parents[1], 'AdventOfCode-Input')
-    INPUT_FILE = Path(ROOT_DIR, '2023/day01.txt')
-
-    start_time = time.perf_counter()
-    with open(INPUT_FILE, 'r') as file:
-        main(file.read().strip('\n'))
-    end_time = time.perf_counter()
-    print(f"Total time (ms): {1000 * (end_time - start_time)}")
+        return p1, p2
---
+++ tests/y2023/test_day01.py
@@ -0,0 +1,27 @@
+TEST_STRING_1 = """1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet"""
+
+TEST_STRING_2 = """two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen"""
+
+from aoc_py.y2023.day01 import solve_parts
+
+# ----------- Part 1 ------------
+
+def test_part1_1() -> None:
+    p1, _ = solve_parts(TEST_STRING_1, 1)
+    assert p1 == "142"
+
+
+# ----------- Part 2 ------------
+
+def test_part2_1() -> None:
+    _, p2 = solve_parts(TEST_STRING_2, 2)
+    assert p2 == "281"
