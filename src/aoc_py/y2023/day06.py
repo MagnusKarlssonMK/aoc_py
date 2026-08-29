c1f5ea050c0dabe0ca15e79b6c9519cab2496d12
MagnusKarlssonMK <magnus.karlsson.mk@gmail.com>
2026-08-29 16:55:23 +0200

    2023 day 6 migrated.

--- src/aoc_py/y2023/day06.py
+++ src/aoc_py/y2023/day06.py
@@ -1,13 +1,12 @@
 """
+2023 day 6 - Wait For It
+
 Uses quadratic formula to calculate the two points where the score intersects the old record. One of the surprising
 challenges was to round them off in the right direction, to also account for the few cases where the solution was
 exactly the same value as the old record.
 """
-
-import time
-from pathlib import Path
-from dataclasses import dataclass
 import math
+from dataclasses import dataclass
 
 
 @dataclass(frozen=True)
@@ -36,28 +35,22 @@ class InputData:
             Race(timelist[idx], distancelist[idx]) for idx, _ in enumerate(timelist)
         ]
 
-    def solve_part1(self) -> int:
+    def get_p1(self) -> int:
         return math.prod([race.get_score() for race in self.__races])
 
-    def solve_part2(self) -> int:
+    def get_p2(self) -> int:
         t = int("".join([str(race.time) for race in self.__races]))
         d = int("".join([str(race.distance) for race in self.__races]))
         megarace = Race(t, d)
         return megarace.get_score()
 
 
-def main(aoc_input: str) -> None:
-    myrace = InputData(aoc_input)
-    print(f"Part 1: {myrace.solve_part1()}")
-    print(f"Part 2: {myrace.solve_part2()}")
+def solve_parts(inputdata: str, part: int | None = None) -> tuple[str, str]:
+        p1, p2 = "-1"
+        p = InputData(inputdata)
+        if part in (None, 1):
+            p1 = str(p.get_p1())
+        if part in (None, 2):
+            p2 = str(p.get_p2())
 
-
-if __name__ == "__main__":
-    ROOT_DIR = Path(Path(__file__).parents[1], "AdventOfCode-Input")
-    INPUT_FILE = Path(ROOT_DIR, "2023/day06.txt")
-
-    start_time = time.perf_counter()
-    with open(INPUT_FILE, "r") as file:
-        main(file.read().strip("\n"))
-    end_time = time.perf_counter()
-    print(f"Total time (ms): {1000 * (end_time - start_time)}")
+        return p1, p2
