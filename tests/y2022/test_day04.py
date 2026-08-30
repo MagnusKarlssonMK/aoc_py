22983fefbd6bd2b674515a25a70a5985709b3324
MagnusKarlssonMK <magnus.karlsson.mk@gmail.com>
2026-08-30 22:50:20 +0200

    2022 day 4 migrated.

---
+++ tests/y2022/test_day04.py
@@ -0,0 +1,21 @@
+TEST_STRING = """2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8"""
+
+from aoc_py.y2022.day04 import solve_parts
+
+# ----------- Part 1 ------------
+
+def test_part1_1() -> None:
+    p1, _ = solve_parts(TEST_STRING, 1)
+    assert p1 == "2"
+
+
+# ----------- Part 2 ------------
+
+def test_part2_1() -> None:
+    _, p2 = solve_parts(TEST_STRING, 2)
+    assert p2 == "4"
