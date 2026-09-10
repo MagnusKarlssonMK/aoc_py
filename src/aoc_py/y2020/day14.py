5f928f6b953e5eac7591bca8860ed857792e663d
MagnusKarlssonMK <magnus.karlsson.mk@gmail.com>
2026-09-10 09:00:46 +0200

    2020 day 14 migrated.

--- src/aoc_py/y2020/day14.py
+++ src/aoc_py/y2020/day14.py
@@ -1,46 +1,45 @@
 """
-Bitmask stuff and write into emulated memory. Not much to say.
+2020 day 14 - Docking Data
 """
-import time
-from pathlib import Path
+
 from dataclasses import dataclass
 from enum import Enum
 
 
 class Action(Enum):
-    MEM = 'mem'
-    MASK = 'mask'
+    MEM = "mem"
+    MASK = "mask"
 
 
 @dataclass(frozen=True)
 class Instruction:
     action: Action
     val1: str
-    val2: str = ''
+    val2: str = ""
 
 
-class Computer:
+class InputData:
     def __init__(self, rawstr: str) -> None:
         self.__instr: list[Instruction] = []
         for line in rawstr.splitlines():
-            left, right = line.split(' = ')
-            if left[:4] == 'mask':
+            left, right = line.split(" = ")
+            if left[:4] == "mask":
                 self.__instr.append(Instruction(Action.MASK, right))
             else:
-                self.__instr.append(Instruction(Action.MEM, left[4:].strip(']'), right))
+                self.__instr.append(Instruction(Action.MEM, left[4:].strip("]"), right))
 
-    def get_memorysum(self) -> int:
+    def get_p1(self) -> int:
         memory: dict[int, int] = {}
         bitmask = 0, 0
         for instr in self.__instr:
             if instr.action == Action.MASK:
-                mask1 = mask2 = ''
+                mask1 = mask2 = ""
                 for c in instr.val1:
-                    if c == 'X':
-                        mask1 += '1'
-                        mask2 += '0'
+                    if c == "X":
+                        mask1 += "1"
+                        mask2 += "0"
                     else:
-                        mask1 += '0'
+                        mask1 += "0"
                         mask2 += c
                 bitmask = int(mask1, 2), int(mask2, 2)
             else:
@@ -49,18 +48,18 @@ class Computer:
                 memory[addr] = (val & bitmask[0]) | bitmask[1]
         return sum(list(memory.values()))
 
-    def get_memorysum_v2(self) -> int:
+    def get_p2(self) -> int:
         memory: dict[int, int] = {}
         xmask = 0
         addrmasks = []
         for instr in self.__instr:
             if instr.action == Action.MASK:
-                x = ''
+                x = ""
                 for c in instr.val1:
-                    if c == 'X':
-                        x += '0'
+                    if c == "X":
+                        x += "0"
                     else:
-                        x += '1'
+                        x += "1"
                 xmask = int(x, 2)
                 addrmasks = self.__mask_value(instr.val1)
             elif instr.action == Action.MEM:
@@ -71,25 +70,20 @@ class Computer:
         return sum(list(memory.values()))
 
     def __mask_value(self, val: str) -> list[int]:
-        if (idx := val.find('X')) != -1:
-            return (self.__mask_value(val[:idx] + '0' + val[idx + 1:]) +
-                    self.__mask_value(val[:idx] + '1' + val[idx + 1:]))
+        if (idx := val.find("X")) != -1:
+            return self.__mask_value(
+                val[:idx] + "0" + val[idx + 1 :]
+            ) + self.__mask_value(val[:idx] + "1" + val[idx + 1 :])
         else:
             return [int(val, 2)]
 
 
-def main(aoc_input: str) -> None:
-    computer = Computer(aoc_input)
-    print(f"Part 1: {computer.get_memorysum()}")
-    print(f"Part 2: {computer.get_memorysum_v2()}")
+def solve_parts(inputdata: str, part: int | None = None) -> tuple[str, str]:
+    p1, p2 = "-1"
+    p = InputData(inputdata)
+    if part in (None, 1):
+        p1 = str(p.get_p1())
+    if part in (None, 2):
+        p2 = str(p.get_p2())
 
-
-if __name__ == "__main__":
-    ROOT_DIR = Path(Path(__file__).parents[1], 'AdventOfCode-Input')
-    INPUT_FILE = Path(ROOT_DIR, '2020/day14.txt')
-
-    start_time = time.perf_counter()
-    with open(INPUT_FILE, 'r') as file:
-        main(file.read().strip('\n'))
-    end_time = time.perf_counter()
-    print(f"Total time (ms): {1000 * (end_time - start_time)}")
+    return p1, p2
---
+++ tests/y2020/test_day14.py
@@ -0,0 +1,26 @@
+TEST_STRING_1 = """mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
+mem[8] = 11
+mem[7] = 101
+mem[8] = 0"""
+
+TEST_STRING_2 = """mask = 000000000000000000000000000000X1001X
+mem[42] = 100
+mask = 00000000000000000000000000000000X0XX
+mem[26] = 1"""
+
+from aoc_py.y2020.day14 import solve_parts
+
+# ----------- Part 1 ------------
+
+
+def test_part1_1() -> None:
+    p1, _ = solve_parts(TEST_STRING_1, 1)
+    assert p1 == "165"
+
+
+# ----------- Part 2 ------------
+
+
+def test_part2_1() -> None:
+    _, p2 = solve_parts(TEST_STRING_2, 2)
+    assert p2 == "208"
