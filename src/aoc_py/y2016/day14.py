765c975ca1d9c6ab6f22601338d0696b2d8d2877
MagnusKarlssonMK <magnus.karlsson.mk@gmail.com>
2026-09-18 17:45:53 +0200

    2016 day 14 migrated.

--- src/aoc_py/y2016/day14.py
+++ src/aoc_py/y2016/day14.py
@@ -1,4 +1,6 @@
 """
+2016 day 14 - One-Time Pad
+
 Generate md5 hashes one-by-one, and for each
 - check if it has 5 repeated characters, and if so, validate any pending previously found index with 3 repetitions of
 that same character
@@ -8,8 +10,7 @@ add it to the first found character, in case there's more than one triple.
 Use a hasher with a cache for at least some chance of reducing time for the ridiculous number of hashings required for
 Part 2. It's still really quite slow though, Part 2 takes around a minute to complete.
 """
-import time
-from pathlib import Path
+
 import hashlib
 import re
 from functools import lru_cache
@@ -23,7 +24,7 @@ def get_md5(inputstr: str, stretch: int = 0) -> str:
     return result
 
 
-class KeyGenerator:
+class InputData:
     def __init__(self, salt: str) -> None:
         self.__salt = salt
 
@@ -53,18 +54,12 @@ class KeyGenerator:
         return sorted(keys)[nbr - 1]
 
 
-def main(aoc_input: str) -> None:
-    keygen = KeyGenerator(aoc_input)
-    print(f"Part 1: {keygen.get_64th_index()}")
-    print(f"Part 2: {keygen.get_64th_index(2016)}")
+def solve_parts(inputdata: str, part: int | None = None) -> tuple[str, str]:
+    p1 = p2 = "-1"
+    p = InputData(inputdata)
+    if part in (None, 1):
+        p1 = str(p.get_64th_index())
+    if part in (None, 2):
+        p2 = str(p.get_64th_index(2016))
 
-
-if __name__ == "__main__":
-    ROOT_DIR = Path(Path(__file__).parents[1], 'AdventOfCode-Input')
-    INPUT_FILE = Path(ROOT_DIR, '2016/day14.txt')
-
-    start_time = time.perf_counter()
-    with open(INPUT_FILE, 'r') as file:
-        main(file.read().strip('\n'))
-    end_time = time.perf_counter()
-    print(f"Total time (ms): {1000 * (end_time - start_time)}")
+    return p1, p2
