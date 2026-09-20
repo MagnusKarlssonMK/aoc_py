f1c39c6a0f208888659853589447ff330dc2d76b
MagnusKarlssonMK <magnus.karlsson.mk@gmail.com>
2026-09-20 23:45:50 +0200

    2016 day 22 migrated.

--- src/aoc_py/y2016/day22.py
+++ src/aoc_py/y2016/day22.py
@@ -1,10 +1,14 @@
 """
-Part 1: Just parse the data into a dict representing the nodes keyed with (x, y) of each node. Then go through the
+2016 day 22 - Grid Computing
+
+Part 1
+
+Just parse the data into a dict representing the nodes keyed with (x, y) of each node. Then go through the
 possible pair combinations of the nodes and count how many are fulfilling the conditions for transfer.
 
-Part 2: Uhhhh....
+Part 2
 
-So.... I guess this is kind of the equivalent of getting Rick-rolled in AoC...
+Uhhhh.... So.... I guess this is kind of the equivalent of getting Rick-rolled in AoC...
 
 All pairs in part 1 contain the same empty node (x=15, y=29 in my case). So for part 2 we first need to move that empty
 node to the top right corner, and the path is partially blocked by a line of full nodes. Once there we can move the
@@ -14,8 +18,7 @@ the grid and then calculate the answer by hand, but to keep it at least a little
 nodes.
 2. Add the row length multiplied by 5 for the cost of moving G to the top left.
 """
-import time
-from pathlib import Path
+
 import re
 from dataclasses import dataclass
 from itertools import combinations
@@ -29,31 +32,31 @@ class Node:
     use: int
 
 
-class Cluster:
-    def __init__(self, rawstr: str) -> None:
+class InputData:
+    def __init__(self, s: str) -> None:
         self.__nodes: dict[tuple[int, int], Node] = {}
         self.__max_x = 0
         self.__max_y = 0
-        self.__zeronode = None
-        for line in rawstr.splitlines():
+        self.__zeronode: tuple[int, int] = -1, -1
+        for line in s.splitlines():
             nbrs = list(map(int, re.findall(r"\d+", line)))
             if len(nbrs) == 6:
                 self.__nodes[(nbrs[0], nbrs[1])] = Node(*nbrs[2:])
                 self.__max_x = max(self.__max_x, nbrs[0])
                 self.__max_y = max(self.__max_y, nbrs[1])
-                if not self.__zeronode and nbrs[3] == 0:
+                if (self.__zeronode == (-1, -1)) and nbrs[3] == 0:
                     self.__zeronode = nbrs[0], nbrs[1]
 
-    def get_viable_pairs_count(self) -> int:
+    def get_p1(self) -> int:
         count = 0
         for a, b in combinations(self.__nodes, 2):
-            if 0 < self.__nodes[a].used < self.__nodes[b].avail:
-                count += 1
-            elif 0 < self.__nodes[b].used < self.__nodes[a].avail:
+            if (0 < self.__nodes[a].used <= self.__nodes[b].avail) or (
+                0 < self.__nodes[b].used <= self.__nodes[a].avail
+            ):
                 count += 1
         return count
 
-    def get_fewest_steps_count(self) -> int:
+    def get_p2(self) -> int:
         # Step 1 - find nbr of steps to move zero-node to G
         g_steps = 0
         node_g = self.__max_x, 0
@@ -69,25 +72,22 @@ class Cluster:
             seen.add(nextnode)
             for d in ((-1, 0), (1, 0), (0, -1)):  # No reason to ever go +1 in y
                 n = nextnode[0] + d[0], nextnode[1] + d[1]
-                if n in self.__nodes and self.__nodes[n].used < self.__nodes[self.__zeronode].size:
+                if (
+                    n in self.__nodes
+                    and self.__nodes[n].used < self.__nodes[self.__zeronode].size
+                ):
                     queue.append((n, steps + 1))
 
         # Step 2 - add the cost for moving G to top left
         return g_steps + (5 * (self.__max_x - 1))
 
 
-def main(aoc_input: str) -> None:
-    cluster = Cluster(aoc_input)
-    print(f"Part 1: {cluster.get_viable_pairs_count()}")
-    print(f"Part 2: {cluster.get_fewest_steps_count()}")
+def solve_parts(inputdata: str, part: int | None = None) -> tuple[str, str]:
+    p1 = p2 = "-1"
+    p = InputData(inputdata)
+    if part in (None, 1):
+        p1 = str(p.get_p1())
+    if part in (None, 2):
+        p2 = str(p.get_p2())
 
-
-if __name__ == "__main__":
-    ROOT_DIR = Path(Path(__file__).parents[1], 'AdventOfCode-Input')
-    INPUT_FILE = Path(ROOT_DIR, '2016/day22.txt')
-
-    start_time = time.perf_counter()
-    with open(INPUT_FILE, 'r') as file:
-        main(file.read().strip('\n'))
-    end_time = time.perf_counter()
-    print(f"Total time (ms): {1000 * (end_time - start_time)}")
+    return p1, p2
