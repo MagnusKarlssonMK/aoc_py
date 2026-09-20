7bdc14b7d073a7f28fd1ce126f4478d64b6650c8
MagnusKarlssonMK <magnus.karlsson.mk@gmail.com>
2026-09-20 21:35:33 +0200

    2016 day 20 migrated.

--- src/aoc_py/y2016/day20.py
+++ src/aoc_py/y2016/day20.py
@@ -1,4 +1,6 @@
 """
+2016 day 20 - Firewall Rules
+
 Parse the input into tuples of low & high and sort the list. The sorting function will default to sort by first
 element (i.e. lowest boundary) with no key specified.
 Then start with a list of values +1 higher than the upper bound of each blocked range, and check one by one if it's
@@ -7,45 +9,43 @@ and try the next.
 Similar deal for part 2, making use of the sorted list and keeping track of the upper bound we've checked for allowed
 numbers while counting numbers not covered by blocked intervals.
 """
-import time
-from pathlib import Path
 
 
-class Firewall:
-    def __init__(self, rawstr: str) -> None:
-        self.__blocklist = sorted([(int(low), int(high)) for low, high in
-                                   [line.split('-') for line in rawstr.splitlines()]])
+class InputData:
+    def __init__(self, s: str) -> None:
+        self.__blocklist = sorted(
+            [
+                (int(low), int(high))
+                for low, high in [line.split("-") for line in s.splitlines()]
+            ]
+        )
 
-    def get_lowest_ip(self) -> int:
+    def get_p1(self) -> int:
         candidate = 0
         for low, high in self.__blocklist:
-            if low <= candidate <= high:
+            if candidate < low:
+                break
+            if candidate <= high:
                 candidate = high + 1
         return candidate
 
-    def get_allowed_ip_count(self) -> int:
+    def get_p2(self) -> int:
         allowed_total = 0
         highest_allowed = 0
         for low, high in self.__blocklist:
             if highest_allowed < low:
                 allowed_total += low - highest_allowed - 1
             highest_allowed = max(high, highest_allowed)
-        allowed_total += 2 ** 32 - 1 - highest_allowed
+        allowed_total += 2**32 - 1 - highest_allowed
         return allowed_total
 
 
-def main(aoc_input: str) -> None:
-    firewall = Firewall(aoc_input)
-    print(f"Part 1: {firewall.get_lowest_ip()}")
-    print(f"Part 2: {firewall.get_allowed_ip_count()}")
+def solve_parts(inputdata: str, part: int | None = None) -> tuple[str, str]:
+    p1 = p2 = "-1"
+    p = InputData(inputdata)
+    if part in (None, 1):
+        p1 = str(p.get_p1())
+    if part in (None, 2):
+        p2 = str(p.get_p2())
 
-
-if __name__ == "__main__":
-    ROOT_DIR = Path(Path(__file__).parents[1], 'AdventOfCode-Input')
-    INPUT_FILE = Path(ROOT_DIR, '2016/day20.txt')
-
-    start_time = time.perf_counter()
-    with open(INPUT_FILE, 'r') as file:
-        main(file.read().strip('\n'))
-    end_time = time.perf_counter()
-    print(f"Total time (ms): {1000 * (end_time - start_time)}")
+    return p1, p2
