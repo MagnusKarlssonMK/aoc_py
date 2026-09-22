6a7b720601e446d786aaff1029f6486d0e24ae7f
MagnusKarlssonMK <magnus.karlsson.mk@gmail.com>
2026-09-22 16:07:54 +0200

    2016 day 24 migrated.

--- src/aoc_py/y2016/day24.py
+++ src/aoc_py/y2016/day24.py
@@ -1,6 +1,8 @@
 """
-Travelling salesman sort of.
-Doing this in multiple steps:
+2016 day 24 - Air Duct Spelunking
+
+Travelling salesman sort of. Doing this in multiple steps:
+
 First set up the grid as an adj list, and find the numbers in the grid while doing that.
 Then find the edges and path lengths between all numbers, giving us a graph of numbers with all the edges, and we can
 throw away the grid after that.
@@ -8,31 +10,29 @@ Based on this we can then do BFS with keeping a sorted list of the visited numbe
 
 For part 2, simply modify the stop condition of the BFS to also require the current point to be 0.
 """
-import time
-from pathlib import Path
 
 
-class AirDucts:
+class InputData:
     def __init__(self, rawstr: str) -> None:
         adj_list: dict[tuple[int, int], set[tuple[int, int]]] = {}
         nbrs: dict[tuple[int, int], int] = {}
         grid = rawstr.splitlines()
         for r, row in enumerate(grid):
             for c, col in enumerate(row):
-                if col != '#':
+                if col != "#":
                     if col.isdigit():
                         nbrs[(r, c)] = int(col)
                     if (r, c) not in adj_list:
                         adj_list[(r, c)] = set()
                     for dr, dc in ((1, 0), (0, 1)):
-                        if grid[r + dr][c + dc] != '#':
+                        if grid[r + dr][c + dc] != "#":
                             adj_list[(r, c)].add((r + dr, c + dc))
                             if (r + dr, c + dc) not in adj_list:
                                 adj_list[(r + dr, c + dc)] = set()
                             adj_list[(r + dr, c + dc)].add((r, c))
         self.__nbrs: dict[int, set[tuple[int, int]]] = {}  # nbr -> {(nbr, steps), ...}
         nbrs_seen: dict[tuple[int, int], tuple[int, bool]] = {}
-        for nbr in nbrs:
+        for nbr, value in nbrs.items():
             points_seen: set[tuple[int, int]] = set()
             queue: list[tuple[tuple[int, int], int, bool]] = [(nbr, 0, False)]
             while queue:
@@ -43,8 +43,10 @@ class AirDucts:
                     continue
                 points_seen.add(point)
                 if point in nbrs and point != nbr:
-                    if (nbrs[nbr], nbrs[point]) not in nbrs_seen or steps < nbrs_seen[(nbrs[nbr], nbrs[point])][0]:
-                        nbrs_seen[(nbrs[nbr], nbrs[point])] = steps, stepover
+                    if (value, nbrs[point]) not in nbrs_seen or steps < nbrs_seen[
+                        (value, nbrs[point])
+                    ][0]:
+                        nbrs_seen[(value, nbrs[point])] = steps, stepover
                     stepover = True
                 for n in adj_list[point]:
                     queue.append((n, steps + 1, stepover))
@@ -57,14 +59,20 @@ class AirDucts:
     def get_shortest_path(self, returntozero: bool = False) -> int:
         seen_states = {}
         shortest_path = None
-        queue: list[tuple[int, int, tuple[int, ...]]] = [(0, 0, (0,))]  # point, steps, seenpoints(sorted)
+        queue: list[tuple[int, int, tuple[int, ...]]] = [
+            (0, 0, (0,))
+        ]  # point, steps, seenpoints(sorted)
         while queue:
             currentpoint, steps, currentseen = queue.pop(0)
-            if (((currentpoint, currentseen) in seen_states and steps >= seen_states[(currentpoint, currentseen)]) or
-                    (shortest_path and steps > shortest_path)):
+            if (
+                (currentpoint, currentseen) in seen_states
+                and steps >= seen_states[(currentpoint, currentseen)]
+            ) or (shortest_path and steps > shortest_path):
                 continue
             seen_states[(currentpoint, currentseen)] = steps
-            if len(currentseen) >= len(self.__nbrs) and (currentpoint == 0 or not returntozero):
+            if len(currentseen) >= len(self.__nbrs) and (
+                currentpoint == 0 or not returntozero
+            ):
                 if shortest_path:
                     shortest_path = min(shortest_path, steps)
                 else:
@@ -79,18 +87,12 @@ class AirDucts:
         return shortest_path
 
 
-def main(aoc_input: str) -> None:
-    grid = AirDucts(aoc_input)
-    print(f"Part 1: {grid.get_shortest_path()}")
-    print(f"Part 2: {grid.get_shortest_path(True)}")
+def solve_parts(inputdata: str, part: int | None = None) -> tuple[str, str]:
+    p1 = p2 = "-1"
+    p = InputData(inputdata)
+    if part in (None, 1):
+        p1 = str(p.get_shortest_path())
+    if part in (None, 2):
+        p2 = str(p.get_shortest_path(True))
 
-
-if __name__ == "__main__":
-    ROOT_DIR = Path(Path(__file__).parents[1], 'AdventOfCode-Input')
-    INPUT_FILE = Path(ROOT_DIR, '2016/day24.txt')
-
-    start_time = time.perf_counter()
-    with open(INPUT_FILE, 'r') as file:
-        main(file.read().strip('\n'))
-    end_time = time.perf_counter()
-    print(f"Total time (ms): {1000 * (end_time - start_time)}")
+    return p1, p2
---
+++ tests/y2016/test_day24.py
@@ -0,0 +1,22 @@
+TEST_STRING = """###########
+#0.1.....2#
+#.#######.#
+#4.......3#
+###########"""
+
+from aoc_py.y2016.day24 import solve_parts
+
+# ----------- Part 1 ------------
+
+
+def test_part1_1() -> None:
+    p1, _ = solve_parts(TEST_STRING, 1)
+    assert p1 == "14"
+
+
+# ----------- Part 2 ------------
+
+
+def test_part2_1() -> None:
+    _, p2 = solve_parts(TEST_STRING, 2)
+    assert p2 == "20"
