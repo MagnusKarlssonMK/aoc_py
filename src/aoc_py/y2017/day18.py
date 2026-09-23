4e6645de24f73f65ba3e5f788d0eb63e9921d1d8
MagnusKarlssonMK <magnus.karlsson.mk@gmail.com>
2026-09-23 23:55:15 +0200

    2017 day 18 migrated.

--- src/aoc_py/y2017/day18.py
+++ src/aoc_py/y2017/day18.py
@@ -1,19 +1,30 @@
 """
+2017 day 18 - Duet
+
 Another assembly simulator of sorts. Nothing special about part 1, but a bit more tricky with part 2 since we now have
 two programs running in parallel and communcicating with messages. Mostly just a matter of having two instances of
 everything, keeping track of the progam states and toggling the 'running' program.
 """
-import time
-from pathlib import Path
+
 from dataclasses import dataclass
 from enum import Enum
 
 
+class Operation(Enum):
+    SND = "snd"
+    SET = "set"
+    ADD = "add"
+    MUL = "mul"
+    MOD = "mod"
+    RCV = "rcv"
+    JGZ = "jgz"
+
+
 @dataclass(frozen=True)
 class Instr:
-    op: str
-    attr1: str = None
-    attr2: str = None
+    op: Operation
+    attr1: str = ""
+    attr2: str = ""
 
 
 class State(Enum):
@@ -25,7 +36,7 @@ class State(Enum):
 class Register:
     def __init__(self, regs: set[str], pval: int) -> None:
         self.__regs: dict[str, int] = {r: 0 for r in regs}
-        self.__regs['p'] = pval
+        self.__regs["p"] = pval
 
     def get_value(self, attr: str) -> int:
         if attr in self.__regs:
@@ -45,106 +56,100 @@ class Register:
         self.__regs[reg] %= val
 
 
-class Duet:
-    def __init__(self, rawstr: str) -> None:
+class InputData:
+    def __init__(self, s: str) -> None:
         self.__registers: set[str] = set()
         self.__instr: list[Instr] = []
-        for line in rawstr.splitlines():
+        for line in s.splitlines():
             t = line.split()
-            self.__instr.append(Instr(*t))
+            self.__instr.append(Instr(Operation(t[0]), *t[1:]))
             for w in t[1:]:
                 if not w[-1].isdigit():
                     self.__registers.add(w)
 
-    def get_recovered_freq(self) -> int:
+    def get_p1(self) -> int:
         sp = 0
         regs = Register(self.__registers, 0)
         sound = None
         while 0 <= sp < len(self.__instr):
             i = self.__instr[sp]
             match i.op:
-                case 'snd':
+                case Operation.SND:
                     sound = regs.get_value(i.attr1)
-                case 'set':
+                case Operation.SET:
                     regs.set_value(i.attr1, regs.get_value(i.attr2))
-                case 'add':
+                case Operation.ADD:
                     regs.add_value(i.attr1, regs.get_value(i.attr2))
-                case 'mul':
+                case Operation.MUL:
                     regs.mul_value(i.attr1, regs.get_value(i.attr2))
-                case 'mod':
+                case Operation.MOD:
                     regs.mod_value(i.attr1, regs.get_value(i.attr2))
-                case 'rcv':
+                case Operation.RCV:
                     if regs.get_value(i.attr1) != 0 and sound:
                         return sound
-                case 'jgz':
+                case Operation.JGZ:
                     if regs.get_value(i.attr1) > 0:
                         sp += regs.get_value(i.attr2)
                         continue
-                case _:
-                    pass
             sp += 1
         return -1
 
-    def get_duet_send_count(self) -> int:
+    def get_p2(self) -> int:
         sp = [0, 0]
         regs = [Register(self.__registers, 0), Register(self.__registers, 1)]
         states = [State.IDLE, State.IDLE]
         inbox: list[list[int]] = [[], []]
         running = 0
         send_count = 0
-        while states[running] == State.IDLE or (states[running] == State.RECEIVING and inbox[running]):
+        while states[running] == State.IDLE or (
+            states[running] == State.RECEIVING and inbox[running]
+        ):
             i = self.__instr[sp[running]]
             match i.op:
-                case 'snd':
+                case Operation.SND:
                     inbox[(running + 1) % 2].append(regs[running].get_value(i.attr1))
                     sp[running] += 1
                     if running == 1:
                         send_count += 1
-                case 'set':
+                case Operation.SET:
                     regs[running].set_value(i.attr1, regs[running].get_value(i.attr2))
                     sp[running] += 1
-                case 'add':
+                case Operation.ADD:
                     regs[running].add_value(i.attr1, regs[running].get_value(i.attr2))
                     sp[running] += 1
-                case 'mul':
+                case Operation.MUL:
                     regs[running].mul_value(i.attr1, regs[running].get_value(i.attr2))
                     sp[running] += 1
-                case 'mod':
+                case Operation.MOD:
                     regs[running].mod_value(i.attr1, regs[running].get_value(i.attr2))
                     sp[running] += 1
-                case 'rcv':
+                case Operation.RCV:
                     if inbox[running]:
-                        regs[running].set_value(i.attr1, regs[running].get_value(inbox[running].pop(0)))
+                        regs[running].set_value(
+                            i.attr1, regs[running].get_value(str(inbox[running].pop(0)))
+                        )
                         states[running] = State.IDLE
                         sp[running] += 1
                     else:
                         states[running] = State.RECEIVING
                         running = (running + 1) % 2
-                case 'jgz':
+                case Operation.JGZ:
                     if regs[running].get_value(i.attr1) > 0:
                         sp[running] += regs[running].get_value(i.attr2)
                     else:
                         sp[running] += 1
-                case _:
-                    pass
             if not 0 <= sp[running] < len(self.__instr):
                 states[running] = State.DONE
                 running = (running + 1) % 2
         return send_count
 
 
-def main(aoc_input: str) -> None:
-    duet = Duet(aoc_input)
-    print(f"Part 1: {duet.get_recovered_freq()}")
-    print(f"Part 2: {duet.get_duet_send_count()}")
+def solve_parts(inputdata: str, part: int | None = None) -> tuple[str, str]:
+    p1 = p2 = "-1"
+    p = InputData(inputdata)
+    if part in (None, 1):
+        p1 = str(p.get_p1())
+    if part in (None, 2):
+        p2 = str(p.get_p2())
 
-
-if __name__ == "__main__":
-    ROOT_DIR = Path(Path(__file__).parents[1], 'AdventOfCode-Input')
-    INPUT_FILE = Path(ROOT_DIR, '2017/day18.txt')
-
-    start_time = time.perf_counter()
-    with open(INPUT_FILE, 'r') as file:
-        main(file.read().strip('\n'))
-    end_time = time.perf_counter()
-    print(f"Total time (ms): {1000 * (end_time - start_time)}")
+    return p1, p2
---
+++ tests/y2017/test_day18.py
@@ -0,0 +1,35 @@
+TEST_STRING_1 = """set a 1
+add a 2
+mul a a
+mod a 5
+snd a
+set a 0
+rcv a
+jgz a -1
+set a 1
+jgz a -2"""
+
+TEST_STRING_2 = """snd 1
+snd 2
+snd p
+rcv a
+rcv b
+rcv c
+rcv d"""
+
+from aoc_py.y2017.day18 import solve_parts
+
+# ----------- Part 1 ------------
+
+
+def test_part1_1() -> None:
+    p1, _ = solve_parts(TEST_STRING_1, 1)
+    assert p1 == "4"
+
+
+# ----------- Part 2 ------------
+
+
+def test_part2_1() -> None:
+    _, p2 = solve_parts(TEST_STRING_2, 2)
+    assert p2 == "3"
