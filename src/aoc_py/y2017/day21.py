10e5d3ddca0cdc046303b545231c19eb8e5acb41
MagnusKarlssonMK <magnus.karlsson.mk@gmail.com>
2026-09-24 12:14:19 +0200

    2017 day 21 migrated.

--- src/aoc_py/y2017/day21.py
+++ src/aoc_py/y2017/day21.py
@@ -1,4 +1,6 @@
 """
+2017 day 21 - Fractal Art
+
 Parts 1 & 2 are basically the same, Part 2 is pretty much just a stress test to see if the solution scales well with
 larger numbers.
 Takes the input and expands it into a dictionary of all possible combinations of 2- and 3-sized input blocks, and from
@@ -6,30 +8,36 @@ there it's mostly down to doing the necessary transformations for each step.
 For a rainy day: a bit curious if it could run even faster by using recursion instead and expanding the dictionary
 sort of like a memo cache along the way for larger block sizes?
 """
-import time
-from pathlib import Path
+
+from typing import Final
 
 
 def flipblock(inblock: tuple[str, ...]) -> tuple[str, ...]:
-    return tuple([''.join(reversed(b)) for b in inblock])
+    return tuple(["".join(reversed(b)) for b in inblock])
 
 
 def rotateblock(inblock: tuple[str, ...]) -> tuple[str, ...]:
-    return tuple([''.join([inblock[row][col] for row in range(len(inblock))])
-                  for col in reversed(range(len(inblock)))])
+    return tuple(
+        [
+            "".join([inblock[row][col] for row in range(len(inblock))])
+            for col in reversed(range(len(inblock)))
+        ]
+    )
 
 
-class ImageConverter:
-    __START_IMAGE = ('.#.', '..#', '###')
+class InputData:
+    __START_IMAGE: Final = (".#.", "..#", "###")
 
     def __init__(self, rawstr: str) -> None:
         self.__transforms: dict[tuple[str, ...], tuple[str, ...]] = {}
         for line in rawstr.splitlines():
-            left, right = line.split(' => ')
-            inp1 = tuple(left.split('/'))  # Work with tuples to make it hashable and possible to use as dict key
+            left, right = line.split(" => ")
+            inp1 = tuple(
+                left.split("/")
+            )  # Work with tuples to make it hashable and possible to use as dict key
             # Pre-compute the possible permutations of the left side
             inp2 = flipblock(inp1)
-            out = right.split('/')
+            out = tuple(right.split("/"))
             self.__transforms[inp1] = out
             self.__transforms[inp2] = out
             for _ in range(3):
@@ -39,35 +47,31 @@ class ImageConverter:
                 self.__transforms[inp2] = out
 
     def get_pixel_count(self, iterations: int) -> int:
-        image = list(ImageConverter.__START_IMAGE)
+        image = list(InputData.__START_IMAGE)
         for _ in range(iterations):
             new_image: list[str] = []
             stepsize = 2 if len(image) % 2 == 0 else 3
             for row in range(0, len(image), stepsize):
-                for _ in range(0, stepsize + 1):
-                    new_image.append('')
+                for _ in range(stepsize + 1):
+                    new_image.append("")
                 for col in range(0, len(image), stepsize):
-                    block = [image[row + i][col: col + stepsize] for i in range(stepsize)]
+                    block = [
+                        image[row + i][col : col + stepsize] for i in range(stepsize)
+                    ]
                     transformed_block = self.__transforms[tuple(block)]
                     target_row = (row // stepsize) * (stepsize + 1)
                     for i in range(stepsize + 1):
                         new_image[target_row + i] += transformed_block[i]
             image = new_image
-        return ''.join(image).count('#')
+        return "".join(image).count("#")
 
 
-def main(aoc_input: str) -> None:
-    converter = ImageConverter(aoc_input)
-    print(f"Part 1: {converter.get_pixel_count(5)}")
-    print(f"Part 1: {converter.get_pixel_count(18)}")
+def solve_parts(inputdata: str, part: int | None = None) -> tuple[str, str]:
+    p1 = p2 = "-1"
+    p = InputData(inputdata)
+    if part in (None, 1):
+        p1 = str(p.get_pixel_count(5))
+    if part in (None, 2):
+        p2 = str(p.get_pixel_count(18))
 
-
-if __name__ == "__main__":
-    ROOT_DIR = Path(Path(__file__).parents[1], 'AdventOfCode-Input')
-    INPUT_FILE = Path(ROOT_DIR, '2017/day21.txt')
-
-    start_time = time.perf_counter()
-    with open(INPUT_FILE, 'r') as file:
-        main(file.read().strip('\n'))
-    end_time = time.perf_counter()
-    print(f"Total time (ms): {1000 * (end_time - start_time)}")
+    return p1, p2
---
+++ tests/y2017/test_day21.py
@@ -0,0 +1,18 @@
+# from aoc_py.y2017.day21 import solve_parts
+
+# No test vectors given
+
+# ----------- Part 1 ------------
+
+
+# def test_part1_1() -> None:
+#    p1, _ = solve_parts(TEST_STRING_1, 1)
+#    assert p1 == "18"
+
+
+# ----------- Part 2 ------------
+
+
+# def test_part2_1() -> None:
+#    _, p2 = solve_parts(TEST_STRING_2, 2)
+#    assert p2 == "9"
